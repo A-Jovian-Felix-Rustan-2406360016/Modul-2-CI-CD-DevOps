e3e665b1a09fb42855238a391808c7725acdba63
Jovian12 <infernum212@gmail.com>
2026-02-23 19:13:35 +0700

    Fix unused import issue on sonarcloud

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
