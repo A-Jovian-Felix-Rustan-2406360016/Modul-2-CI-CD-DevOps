6bcc9b9072019c9194fb4e0bcd4210f0c79bc0f3
Jovian12 <infernum212@gmail.com>
2026-03-01 00:09:46 +0700

    Add html

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -71,11 +71,12 @@ class CarController extends ProductController {
         super(productService);
     }
 
+
     @GetMapping("/createCar")
     public String createCarPage (Model model) {
         Car car = new Car();
         model.addAttribute("car", car);
-        return "createCar";
+        return "CreateCar";
     }
 
     @PostMapping("/createCar")
@@ -88,14 +89,14 @@ class CarController extends ProductController {
     public String carListPage(Model model) {
         List<Car> allCars = carservice.findAll();
         model.addAttribute("cars", allCars);
-        return "carList";
+        return "CarList";
     }
 
     @GetMapping ("/editCar/{carId}")
     public String editCarPage(@PathVariable String carId, Model model) {
         Car car = carservice.findById(carId);
         model.addAttribute("car", car);
-        return "editCar";
+        return "EditCar";
     }
 
     @PostMapping("/editCar")
